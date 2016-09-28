505677e2b87b51863fb7b2a7d8558be80a14708f
Elijah Lofgren <elijahlofgren@gmail.com>
2016-09-27 20:22:51 -0500

    Added config for Heroku

---
+++ api/controllers/UserController.js
@@ -0,0 +1,61 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.delet()`
+   */
+  delet: function (req, res) {
+    return res.json({
+      todo: 'delet() is not implemented yet!'
+    });
+  }
+};
+
